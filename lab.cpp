4735eaca9a563acb961628449a0b49696c0bd224
Юрков Тимофій Олександрович <145855201+Timofii0507@users.noreply.github.com>
2023-11-23 15:36:22 +0200

    Update lab.cpp

--- lab.cpp
+++ lab.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <Windows.h>
+#include <conio.h>
 using namespace std;
 
 int main()
@@ -10,7 +11,7 @@ int main()
 	double profits[mounths];
 		for (size_t i = 0; i < mounths; i++)
 		{
-			cout << "Ââåä³òü ïðèáóòîê  çà " << i + 1 << "-é ì³ñÿöü: ";
+			cout << "Ã‚Ã¢Ã¥Ã¤Â³Ã²Ã¼ Ã¯Ã°Ã¨Ã¡Ã³Ã²Ã®Ãª  Ã§Ã  " << i + 1 << "-Ã© Ã¬Â³Ã±Ã¿Ã¶Ã¼: ";
 			cin >> profits[i];
 		}
 	double totalProfit = 0.0;
@@ -19,7 +20,7 @@ int main()
 		{
 			totalProfit += profits[i];
 		}
-		cout << "Çàãàëüíèé  ïðèáóòîê  çà 6  ì³ñÿö³â: " << totalProfit << endl;
+		cout << "Ã‡Ã Ã£Ã Ã«Ã¼Ã­Ã¨Ã©  Ã¯Ã°Ã¨Ã¡Ã³Ã²Ã®Ãª  Ã§Ã  6  Ã¬Â³Ã±Ã¿Ã¶Â³Ã¢: " << totalProfit << endl;
 	}
 	return  0;
 }
